31bb3236b8c97351aa758d6708badaf23e5ddac8
Waset <55943270+waset@users.noreply.github.com>
2022-04-16 11:24:54 +0800

    chore(instructions): 🔧 更新使用说明 (#5)
    * docs(config): 📝 默认配置
    
    * docs(readme): 📝 更新使用说明

--- config/config.php
+++ config/config.php
@@ -1,13 +1,36 @@
 <?php
 
 // +----------------------------------------------------------------------
-// | 平台设置
+// | 平台配置
 // +----------------------------------------------------------------------
 
 return [
-    // 字节跳动开放平台(抖音、头条、西瓜)
-    "bytedance" =>  [
-        "client_key"    =>  "",
-        "client_secret" =>  "",
+    // 抖音
+    "douyin" => [
+        'client_key'    => env('douyin.client_key', ''),
+        'client_secret' => env('douyin.client_secret', ''),
+        'scope'         => env('douyin.scope', [
+            'trial.whitelist',
+            'user_info',
+            // ...
+        ]),
+    ],
+    // 头条
+    "toutiao" => [
+        'client_key'    => env('toutiao.client_key', ''),
+        'client_secret' => env('toutiao.client_secret', ''),
+        'scope'         => env('toutiao.scope', [
+            'user_info',
+            // ...
+        ])
+    ],
+    //  西瓜
+    "xigua" => [
+        'client_key'    => env('xigua.client_key', ''),
+        'client_secret' => env('xigua.client_secret', ''),
+        'scope'         => env('xigua.scope', [
+            'user_info',
+            // ...
+        ])
     ],
 ];
